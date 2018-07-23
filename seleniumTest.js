d6b76fef022339307dad5aab9d51574498b8b0ed
denis.podgorny <denispodgorny@gmail.com>
2018-07-23 13:10:17 +0300

    Changed promises logic to not use subpromises

--- seleniumTest.js
+++ seleniumTest.js
@@ -4,38 +4,35 @@ let driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
-driver.get("https://www.tut.by");
+let tableValueStr;
 
+driver.get("https://www.tut.by");
 driver.findElement(webdriver.By.xpath("//a[@title='Финансы']")).click()
     .then(() => {
-
         return driver.wait(webdriver.until.elementLocated(webdriver.By.xpath("//a[@data-ua-hash='finance.tut.by_main_left_logo_tut']")));
     })
     .then(() => {
-
-        driver.findElement(webdriver.By.css("input[data-currency='BYN']")).sendKeys("2000");
+        return driver.findElement(webdriver.By.css("input[data-currency='BYN']")).sendKeys("2000");
     })
     .then(() => {
-
-        driver.findElement(webdriver.By.css("input[data-currency='USD']")).getAttribute('value')
-            .then((tableValueStr) => {
-                driver.findElement(webdriver.By.css(".w-currency_table tr:nth-child(1) td:nth-child(4) p")).getText()
-                    .then((rate) => {
-
-                        let countedValue = Math.round(2000/rate);
-                        let tableValue = parseInt(tableValueStr.replace(/ /g, ""));
-
-                        if (countedValue === tableValue) {
-
-                            console.log('Test Passed\n' + 'Value from table = ' + tableValue + ' equal to counted value = ' + countedValue);
-                        } else {
-
-                            console.log('Test Failed\n' + 'Value from table = ' + tableValue + ' not equal to counted value = ' + countedValue);
-                        }
-                    })
-                    .then(() => {
-
-                        driver.quit()
-                    })
-            })
+        return driver.findElement(webdriver.By.css("input[data-currency='USD']")).getAttribute('value')
     })
+    .then((tableValue) => {
+        tableValueStr = tableValue;
+        return driver.findElement(webdriver.By.css(".w-currency_table tr:nth-child(1) td:nth-child(4) p")).getText()
+    })
+    .then((rate) => {
+
+        let countedValue = Math.round(2000/rate*100)/100;
+        let tableValue = parseFloat(tableValueStr.replace(/ /g, ""));
+
+        if (countedValue === tableValue) {
+
+            console.log('Test Passed\n' + 'Value from table = ' + tableValue + ' equal to counted value = ' + countedValue);
+            } else {
+
+            throw new Error(console.log('Test Failed\n' + 'Value from table = ' + tableValue + ' not equal to counted value = ' + countedValue));
+            }
+    })
+    .then(() => {driver.quit()}, () => {driver.quit()})
+
