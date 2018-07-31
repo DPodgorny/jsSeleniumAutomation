fffad5c4601371d0dc42f2e817b20810f6f85c42
denis.podgorny <denispodgorny@gmail.com>
2018-07-31 12:36:59 +0300

    Updated code to use protractor instead of webdriver on non-Angular pages.

--- spec.js
+++ spec.js
@@ -1,33 +1,41 @@
 describe('Homepage', function() {
 
-    it('login has validation', function () {
-        browser.get('https://www.lego.com')
-            .then(
-                element(by.css("area[alt='No']")).click()
-                    .then(function() {return;}, function() {return;})
-            )
-            .then(element(by.css("a[data-uitest='login-link']")).click())
-            .then(browser.switchTo().frame(element(by.id('legoid-iframe')).getWebElement()))
-            .then(browser.driver.findElement(by.css("button[id='buttonSubmitLogin']")).click());
-
-            var errorName = browser.driver.findElement(by.css("p[for='fieldUsername']"));
-            var errorPassword = browser.driver.findElement(by.css("p[for='fieldPassword']"));
-
-            expect(errorName.getText()).toEqual('Enter your username');
-            expect(errorPassword.getText()).toEqual('Enter your password');
-
-            browser.switchTo().defaultContent()
-                .then(element(by.className('legoid-close')).click())
+    beforeEach (function() {
+        browser.ignoreSynchronization = false;
+        return browser.get('https://www.lego.com');
     });
 
-    it('search is performed', function() {
-        browser.get('https://www.lego.com')
-            .then(element(by.css(".l-gh__main-wrapper button[class='l-search__toggle']")).click())
-            .then(element(by.css(".l-gh__main-wrapper input[type='search']")).sendKeys('Ninjago'))
-            .then(element(by.css(".l-gh__main-wrapper button[type='submit'")).click());
+    it('login has validation', () => {
 
-        var product = browser.driver.findElement(by.css("a[href='//shop.lego.com/en-US/Lloyd-Spinjitzu-Master-70628'] span"));
+        var errorName = element(by.css("p[for='fieldUsername']"));
+        var errorPassword = element(by.css("p[for='fieldPassword']"));
 
-        expect(product.getText()).toEqual('Lloyd - Spinjitzu Master');
-    })
+        return element(by.css("area[alt='No']")).click()
+            .then(() => {return;}, () => {return;})
+            .then(() => element(by.css("a[data-uitest='login-link']")).click())
+            .then(() => {
+                browser.ignoreSynchronization = true;
+                browser.switchTo().frame(element(by.id('legoid-iframe')).getWebElement());
+            })
+            .then(() => element(by.css("button[id='buttonSubmitLogin']")).click())
+            .then(() => {
+                expect(errorName.getText()).toEqual('Enter your username');
+                expect(errorPassword.getText()).toEqual('Enter your password');
+            })
+            .then(() => browser.switchTo().defaultContent())
+            .then(() => element(by.className('legoid-close')).click())
+    });
+
+    it('search is performed', () => {
+
+        var product = element(by.css("a[href='//shop.lego.com/en-US/Lloyd-Spinjitzu-Master-70628'] span"));
+
+        return element(by.css(".l-gh__main-wrapper button[class='l-search__toggle']")).click()
+            .then(() => element(by.css(".l-gh__main-wrapper input[type='search']")).sendKeys('Ninjago'))
+            .then(() => element(by.css(".l-gh__main-wrapper button[type='submit'")).click())
+            .then(() => {
+                browser.ignoreSynchronization = true;
+                expect(product.getText()).toEqual('Lloyd - Spinjitzu Master');
+            })
+    });
 })
