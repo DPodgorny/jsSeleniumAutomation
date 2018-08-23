8c61f7a348d93bc217177b95c6e3a5accdf8f8cb
denis.podgorny <denispodgorny@gmail.com>
2018-08-23 13:01:13 +0300

    Updated file names

--- Tests/Cucumber/steps/mainPage-steps.js
+++ Tests/Cucumber/steps/mainPage-steps.js
@@ -7,7 +7,6 @@ let expect = require('chai').expect;
 let assert = require('assert')
 
 Given(/^I open '(.+)'$/, async function(page) {
-
     pageFactory.currentPage = pageFactory[page];
     await pageFactory.currentPage.open();
     await pageFactory.currentPage.skipSurvey();
@@ -15,39 +14,30 @@ Given(/^I open '(.+)'$/, async function(page) {
 
 
 Then(/^'(.+)' is visible$/, async function(element) {
-
-    //await expect(mainPage.elementData[element].isPresent()).to.equal(true);
-    //function fun(){mainPage.elementData[element].isPresent().then(function(a){return a;});};
-    //var expected = mainPage.elementData[element];
     let expected = await pageFactory.currentPage.elementData[element].isPresent();
     expect(expected).to.equal(true);
 });
 
 
 When(/^I press '(.+)'$/, async function(button) {
-
     await pageFactory.currentPage.elementData[button].click();
 });
 
 When(/^I search '(.+)' item$/, async function (item) {
-
     pageFactory.currentPage = pageFactory['Search result page'];
     await pageFactory.currentPage.search(item);
 })
 
 Then(/^'(.+)' item is visible$/, async function (item) {
-
     let expected = await pageFactory.currentPage.foundItem(item).isPresent();
     expect(expected).to.equal(true);
 })
 
 When(/^I login with '(.+)' creds$/, async function(creds) {
-
     await pageFactory.currentPage.login(creds);
 })
 
 Then('I see empty login error messages', async function() {
-
     let expectedName = await pageFactory.currentPage.elementData.emptyNameError.isPresent();
     let expectedPass = await pageFactory.currentPage.elementData.emptyPasswordError.isPresent();
     expect(expectedName).to.equal(true);
@@ -55,20 +45,6 @@ Then('I see empty login error messages', async function() {
 })
 
 Then('I see invalid login error message', async function() {
-
     var expected = await pageFactory.currentPage.elementData.invalidCredsError.isPresent();
     expect(expected).to.equal(true);
 })
-/*When('I login', async function() {
-    await mainPage.open();
-})*/
-
-/*const { Given, When, Then } = require('cucumber');
-const webdriver = require('selenium-webdriver');
-let driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .build();
-
-When('I login', async function() {
-    await driver.get('https:tut.by')
-})*/
